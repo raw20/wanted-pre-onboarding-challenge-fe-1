28bd3302730302db2adebc1a157a29773297815d
정찬욱 <raw1250@gmail.com>
2023-01-12 19:54:17 +0900

    js -> ts &&  jsx -> tsx 변경

--- client/src/routes/Router.tsx
+++ client/src/routes/Router.tsx
@@ -1,8 +1,8 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Login from "../pages/Login";
-import SignUp from "../pages/SignUp";
 import Home from "../pages/Home";
+import SignUp from "../pages/SignUp";
 import TodoList from "../pages/TodoList";
 import TodoListView from "../components/TodoList/TodoListView";
 import TodoDetailView from "../components/TodoList/TodoDetailView";
