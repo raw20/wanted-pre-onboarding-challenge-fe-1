3fdd7587d56c591ca8d9b98d2d562cce77416115
정찬욱 <raw1250@gmail.com>
2023-01-14 21:27:04 +0900

    react-query setting

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import "./App.css";
-import { RecoilRoot } from "recoil";
 import Footer from "./components/Footer/Footer";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
 import Router from "./routes/Router";
@@ -9,12 +8,10 @@ const theme = createTheme();
 
 function App() {
   return (
-    <RecoilRoot>
-      <ThemeProvider theme={theme}>
-        <Router />
-        <Footer />
-      </ThemeProvider>
-    </RecoilRoot>
+    <ThemeProvider theme={theme}>
+      <Router />
+      <Footer />
+    </ThemeProvider>
   );
 }
 
